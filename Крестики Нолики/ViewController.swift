0411438f38c57d19cdbebff93336eb8c9201926f
Alex Balabon <balanon7@gmail.com>
2019-04-22 14:28:20 +0300

    Initial Commit

---
+++ Крестики Нолики/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Крестики Нолики
+//
+//  Created by mac on 4/22/19.
+//  Copyright © 2019 sashabalabon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //self.radiusButton.layer.cornerRadius = 10
+        
+    }
+    
+    @IBAction func activeButton(_ sender: Any) {
+    }
+}
+
